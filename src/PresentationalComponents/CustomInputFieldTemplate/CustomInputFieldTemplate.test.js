40dc520ed35c8c8b89c23cec8602ab823a3ac2a1
Sebastian Gräßl <bastilian@users.noreply.github.com>
2019-04-10 20:21:05 +0200

    Add test to verify change handle call (#39)

--- src/PresentationalComponents/CustomInputFieldTemplate/CustomInputFieldTemplate.test.js
+++ src/PresentationalComponents/CustomInputFieldTemplate/CustomInputFieldTemplate.test.js
@@ -3,24 +3,34 @@ import toJson from 'enzyme-to-json';
 import { CustomInputFieldTemplate } from './CustomInputFieldTemplate';
 
 describe('CustomInputFieldTemplate', () => {
-    it('expect to render', () => {
-        const testProps = {
-            formData: 'Test Form Data',
-            schema: {
-                title: 'Test Input Field'
-            },
-            uiSchema: {
-                'ui:placeholder': 'Placeholder text'
-            },
-            onChange: jest.fn(),
-            name: 'test-input-field',
-            required: true
-        };
+    const testProps = {
+        formData: 'Test Form Data',
+        schema: {
+            title: 'Test Input Field'
+        },
+        uiSchema: {
+            'ui:placeholder': 'Placeholder text'
+        },
+        onChange: jest.fn(),
+        name: 'test-input-field',
+        required: true
+    };
 
+    it('expect to render', () => {
         const wrapper = shallow(
             <CustomInputFieldTemplate { ...testProps } />
         );
 
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('expect to call onChange handler', () => {
+        const wrapper = shallow(
+            <CustomInputFieldTemplate { ...testProps } />
+        );
+        const testString = 'Test Input';
+        wrapper.find('TextInput').simulate('change', testString);
+        expect(testProps.onChange).toHaveBeenCalledWith(testString);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
